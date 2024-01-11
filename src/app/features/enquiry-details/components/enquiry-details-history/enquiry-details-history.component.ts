4d3b14a044afb1f2c25a835b23abe9d189ae70ff
Bharat Yadav Golagana <bharat.golagana@gmail.com>
2024-01-11 10:49:42 +0530

    minor chnages to history and details page

--- src/app/features/enquiry-details/components/enquiry-details-history/enquiry-details-history.component.ts
+++ src/app/features/enquiry-details/components/enquiry-details-history/enquiry-details-history.component.ts
@@ -3,56 +3,65 @@ import { process, State } from '@progress/kendo-data-query';
 import { EnquiryDetailsService } from '../../enquiry-details.service';
 import { ActivatedRoute, Router } from '@angular/router';
 interface EnquiryDetailsHistory {
-  remarksID: number,
-  remarks: string,
-  leadLogGUID: number,
-  leadLogDocName: string,
-  leadLogSrcType: string,
-  leadLogDocPath: string,
-  updDate: string,
-  createdByID: number,
-  updatedBy: string,
-  isMultiple: number,
-  modeOfContactId: number,
-  modeOfContact: string
+  remarksID: number;
+  remarks: string;
+  leadLogGUID: number;
+  leadLogDocName: string;
+  leadLogSrcType: string;
+  leadLogDocPath: string;
+  updDate: string;
+  createdByID: number;
+  updatedBy: string;
+  isMultiple: number;
+  modeOfContactId: number;
+  modeOfContact: string;
 }
 @Component({
   selector: 'app-enquiry-details-history',
   templateUrl: './enquiry-details-history.component.html',
-  styleUrls: ['./enquiry-details-history.component.scss']
+  styleUrls: ['./enquiry-details-history.component.scss'],
 })
-export class EnquiryDetailsHistoryComponent implements OnInit{
-  contactCards:EnquiryDetailsHistory[] = [];
-  pageSize = 3; 
+export class EnquiryDetailsHistoryComponent implements OnInit {
+  contactCards: EnquiryDetailsHistory[] = [];
+  pageSize = 3;
   filteredCards: any[] = [];
   skip = 0;
   total = 0;
   searchTerm = '';
-  constructor( private route: ActivatedRoute,private router: Router,private enquiryDetailService: EnquiryDetailsService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private enquiryDetailService: EnquiryDetailsService
+  ) {}
 
   ngOnInit() {
     this.enquiryDetailsHistory();
   }
 
-  enquiryDetailsHistory(){
-    this.enquiryDetailService.getEnquiryDetailsHistory().subscribe((data: any) => {
-      this.contactCards = data;
-      this.filterData();
-    });
+  enquiryDetailsHistory() {
+    this.enquiryDetailService
+      .getEnquiryDetailsHistory()
+      .subscribe((data: any) => {
+        this.contactCards = data;
+        this.filterData();
+      });
   }
 
-
-  
-  
   filterData(): void {
     this.filteredCards = [...this.contactCards];
     if (this.searchTerm.trim() !== '') {
-      this.filteredCards = this.contactCards.filter(a =>
-        (a.updDate && a.updDate.toLowerCase().includes(this.searchTerm.toLowerCase())) ||
-      ( a. modeOfContact&& a. modeOfContact.toLowerCase().includes(this.searchTerm.toLowerCase())) ||
-      (a.updatedBy && a.updatedBy.toLowerCase().includes(this.searchTerm.toLowerCase()))
+      this.filteredCards = this.contactCards.filter(
+        a =>
+          (a.updDate &&
+            a.updDate.toLowerCase().includes(this.searchTerm.toLowerCase())) ||
+          (a.modeOfContact &&
+            a.modeOfContact
+              .toLowerCase()
+              .includes(this.searchTerm.toLowerCase())) ||
+          (a.updatedBy &&
+            a.updatedBy.toLowerCase().includes(this.searchTerm.toLowerCase()))
       );
-    } 
+    }
     this.total = this.filteredCards.length;
     this.applyPagination();
   }
@@ -60,18 +69,19 @@ export class EnquiryDetailsHistoryComponent implements OnInit{
   applyPagination(): void {
     const state: State = {
       skip: this.skip,
-      take: this.pageSize
+      take: this.pageSize,
     };
     const processed = process(this.filteredCards, state);
     this.filteredCards = processed.data;
     this.total = processed.total;
   }
-  
+
   onPageChange(state: State): void {
-    this.skip = state.skip as number
+    this.skip = state.skip as number;
     this.filterData();
+  }
+
+  onBackClick() {
+    window.history.back();
+  }
 }
-
-}
-
-
