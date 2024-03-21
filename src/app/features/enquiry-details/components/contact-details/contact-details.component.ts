2b932fbbd0fb7ab5e77a67f5c399cd33daa944c6
Bharat Yadav Golagana <bharat.golagana@gmail.com>
2024-03-21 10:40:30 +0530

    loader for contact details

--- src/app/features/enquiry-details/components/contact-details/contact-details.component.ts
+++ src/app/features/enquiry-details/components/contact-details/contact-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { EnquiryDetailsService } from '../../enquiry-details.service';
+import { FormStateService } from '../../form-state.service';
 
 type SoleToContact = {
   contactID: number;
@@ -31,16 +32,29 @@ export class ContactDetailsComponent implements OnInit {
   public soldToContact: any = [];
   public soldToSite: unknown = [];
   public regionsList: unknown = [];
-  public soldToContactList:unknown = [];
+  public soldToContactList: unknown = [];
+  public showContactAPILoader = false;
 
-  public soldToSiteDefaultValue :{
+  public soldToSiteDefaultValue: {
     comboType: string;
     leSiteId: unknown;
-  }| null = null;
-  constructor(public enquiryDetailsService: EnquiryDetailsService) {
+  } | null = null;
+  constructor(
+    public enquiryDetailsService: EnquiryDetailsService,
+    private formStateService: FormStateService
+  ) {
     this.soldToContact = this.soldToContact.slice();
   }
   ngOnInit(): void {
+    if (this.formStateService.contactDetailsFormState) {
+      this.contactDetails.patchValue(
+        this.formStateService.contactDetailsFormState
+      );
+    }
+    // Load selected contact if exists
+    if (this.formStateService.selectedContact) {
+      this.handleSoldToContactChanged(this.formStateService.selectedContact);
+    }
     this.enquiryDetailsService.getSoldToContactsList().subscribe(data => {
       this.soldToContact = data;
       this.soldToContactList = data;
@@ -48,45 +62,51 @@ export class ContactDetailsComponent implements OnInit {
   }
 
   handleSoldToContactFilter(contactID: string) {
-    if(contactID && contactID.length >= 1){
-    this.soldToContact = this.soldToContact.filter(
-      (s:SoleToContact) => s.contactName.toLowerCase().indexOf(contactID.toLowerCase())!== -1
-    );
+    if (contactID && contactID.length >= 1) {
+      this.soldToContact = this.soldToContact.filter(
+        (s: SoleToContact) =>
+          s.contactName.toLowerCase().indexOf(contactID.toLowerCase()) !== -1
+      );
+    } else {
+      this.soldToContact = this.soldToContactList;
+    }
   }
-  else{
-    this.soldToContact = this.soldToContactList
-  }
- 
-}
 
   @Input()
   public contactDetails!: FormGroup;
   handleSoldToContactChanged(contact: SoleToContact) {
     if (contact && contact?.contactID) {
+      this.showContactAPILoader = true;
+      this.formStateService.selectedContact = contact;
       this.enquiryDetailsService
         .getSoldToSiteList(contact.contactID)
         .subscribe((res: any) => {
           this.soldToSite = res || '';
-  
-          if (res && res.length > 0) { 
+
+          if (res && res.length > 0) {
             this.soldToSiteDefaultValue = {
               comboType: 'SOLDTOLESITE',
               leSiteId: res[0]?.leSiteID,
             };
-  
+
             this.contactDetails.patchValue({
-              soldToSite: this.soldToSiteDefaultValue.leSiteId
+              soldToSite: this.soldToSiteDefaultValue.leSiteId,
             });
 
             this.handleSoldToSiteChanged(this.soldToSiteDefaultValue);
           }
+        })
+        .add(() => {
+          this.showContactAPILoader = false;
         });
     }
   }
-  
-  handleSoldToSiteChanged(site: any) { 
+
+  handleSoldToSiteChanged(site: any) {
+    this.formStateService.contactDetailsFormState =
+      this.contactDetails.getRawValue();
     this.enquiryDetailsService
-      .getRegionFromSiteList(site.leSiteId) 
+      .getRegionFromSiteList(site.leSiteId)
       .subscribe((res: any) => {
         this.regionsList = res || '';
         this.contactDetails.patchValue({
@@ -97,6 +117,4 @@ export class ContactDetailsComponent implements OnInit {
         this.enquiryDetailsService.leID = res[1]?.comboID;
       });
   }
-  
-
 }
